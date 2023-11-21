2ebe743ec44b842e405e55d4632aa02c7a7611fc
luigilalo <luigilaloe1@gmail.com>
2023-11-21 15:03:09 +0100

    Array created

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/* 
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite nella tabella qui sotto.
+
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+*/
+
+// Creo l'Array di oggetti con le informazioni fornite nella tabella
+
+let team = [
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & CEO"
+    },
+    {
+        name: "Angela",
+        surname: "Carroll",
+        role: "Chief Editor"
+    },
+    {
+        name: "Walter",
+        surname: "Gordon",
+        role: "Office Manager"
+    },
+    {
+        name: "Angela",
+        surname: "Lopez",
+        role: "Social Media Manager"
+    },
+    {
+        name: "Scott",
+        surname: "Estrada",
+        role: "Developer"
+    },
+    {
+        name: "Barbara",
+        surname: "Ramos",
+        role: "Graphic Designer"
+    }
+];
+console.log(team)
+
+// ---------------------------------------- FUNZIONI --------------------------------------
